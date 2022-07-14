5b6450145f2534a7acd64600047f0856e0d0eb96
MeherVivek <nmv.nkll@gmail.com>
2022-07-14 23:27:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Break Fast Menu')
-streamlit.text('Idly')
-streamlit.text('Dosa')
+streamlit.header('Breakfast Menu')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞'Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
